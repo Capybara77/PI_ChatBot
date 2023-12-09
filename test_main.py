9e252f8e41bfd10f8f673f8a21ea842e0688cf51
Alex Epik <73494181+EvgeniyPukanovich@users.noreply.github.com>
2023-12-10 00:11:24 +0500

    new test

--- test_main.py
+++ test_main.py
@@ -8,3 +8,9 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_response_model():
+    response = client.get("/chat?input_data=Hello")
+    assert response.status_code == 200
+    assert response.json()
