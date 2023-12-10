2cb5fbf725612d14733ad397f035a5b914640551
Alex Epik <73494181+EvgeniyPukanovich@users.noreply.github.com>
2023-12-10 23:56:47 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -10,6 +10,12 @@ def test_read_main():
     assert response.json() == {"message": "Hello World"}
 
 
+def test_clear_history():
+    response = client.get("/clear")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Chat history cleared"}
+
+
 def test_response_model():
     response = client.get("/chat?input_data=Hello")
     assert response.status_code == 200
